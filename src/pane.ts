8447b864ab93a6f0d30fee57d449c6bfbd6e0476
Benjamin Morrell <BDMorrell@gmail.com>
2025-04-01 14:20:37 -0600

    Replace corresponding variables with compound

--- src/pane.ts
+++ src/pane.ts
@@ -2,6 +2,12 @@ type Position = {
     x: number;
     y: number;
 };
+
+interface DraggingCtx {
+    pane: HTMLElement,
+    offset_position: Position,
+    pointer_id: number,
+}
 /* Using CSS selector notation:
 
    .pane is a movable window-like thing.
@@ -18,15 +24,13 @@ type Position = {
 
 export default function makePaneDomain(domain: HTMLElement): (event: PointerEvent) => void {
     const dragging_domain = domain;
-    let dragging_pane: HTMLElement | null = null;
-    let dragging_pane_offset_position: Position | null = null;
-    let dragging_pointer: number | null = null;
+    let dragging_context: DraggingCtx | null = null;
 
     function registerCurrentDraggingPane(element: HTMLElement) {
-        if (dragging_pane !== null) {
-            console.debug("Already dragging:", dragging_pane);
+        if (dragging_context !== null) {
+            console.debug("Already dragging:", dragging_context.pane);
         } else {
-            dragging_pane = element;
+            // dragging_context.pane = element;
             document.addEventListener("pointerup", pointerUp);
             document.addEventListener("pointercancel", pointerUp);
             document.addEventListener("lostpointercapture", pointerUp);
@@ -34,25 +38,31 @@ export default function makePaneDomain(domain: HTMLElement): (event: PointerEven
         }
     }
     function pointerUp(event: PointerEvent) {
-        if (dragging_pointer !== null && event.pointerId !== dragging_pointer) { return; }
-        // console.debug("pointerUp");
+        if (dragging_context === null) { console.error("pointerUp fired for a pane that has no dragging_context!"); return; }
+        // Checing the pointer_id can cause any mistake on our end to not be
+        // fixable by the user.
+        // This will cause bugs for multiple pointers, but I would rather have
+        // something clunky then something that forces the user to reload the
+        // page.
+        // If this changes in the future, we better make sure we don't make
+        // any mistakes on our end.
+
         document.removeEventListener("pointerup", pointerUp);
         document.removeEventListener("pointercancel", pointerUp);
         document.removeEventListener("lostpointercapture", pointerUp);
         dragging_domain!.removeEventListener("pointermove", pointerMove);
-        dragging_pane = null;
-        dragging_pane_offset_position = null;
+        dragging_context = null;
         event.stopPropagation();
     }
     function pointerMove(event: PointerEvent) {
-        if (event.pointerId !== dragging_pointer) { return; }
+        if (dragging_context === null) { console.error("pointerMove fired for a pane that has no dragging_context!"); return; }
+        if (event.pointerId !== dragging_context?.pointer_id) { return; }
         event.stopPropagation();
-        console.assert(dragging_pane_offset_position != null && dragging_pane != null);
 
-        const { x, y } = dragging_pane_offset_position!;
+        const { x, y } = dragging_context.offset_position;
 
-        dragging_pane!.style.left = CSS.px(event.clientX - x);
-        dragging_pane!.style.top = CSS.px(event.clientY - y);
+        dragging_context.pane.style.left = CSS.px(event.clientX - x);
+        dragging_context.pane.style.top = CSS.px(event.clientY - y);
     }
     function onPointerDown(event: PointerEvent) {
         if (event.isPrimary && event.currentTarget !== null && event.target !== null) {
@@ -70,14 +80,17 @@ export default function makePaneDomain(domain: HTMLElement): (event: PointerEven
                 }
             }
             if (valid_selection) {
-                registerCurrentDraggingPane(event.currentTarget as HTMLElement);
-                dragging_pointer = event.pointerId;
                 const element_box = (event.target as HTMLElement).getBoundingClientRect();
                 const this_box = (event.currentTarget as HTMLElement).getBoundingClientRect();
                 const domain_box = dragging_domain.getBoundingClientRect();
-                dragging_pane_offset_position = {
-                    x: event.offsetX + (element_box.x - this_box.x + domain_box.x),
-                    y: event.offsetY + (element_box.y - this_box.y + domain_box.y),
+                registerCurrentDraggingPane(event.currentTarget as HTMLElement);
+                dragging_context = {
+                    pane: event.currentTarget as HTMLElement,
+                    offset_position: {
+                        x: event.offsetX + (element_box.x - this_box.x + domain_box.x),
+                        y: event.offsetY + (element_box.y - this_box.y + domain_box.y),
+                    },
+                    pointer_id: event.pointerId,
                 };
                 event.stopPropagation();
             }
