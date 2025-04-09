85a9c270111dbe762d038dea8ffa8f91e9fd40f4
Benjamin Morrell <BDMorrell@gmail.com>
2025-04-09 15:51:55 -0600

    Break out pane-related CSS to a CSS module

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,9 @@ import solid from 'vite-plugin-solid'
 
 export default defineConfig({
   plugins: [solid()],
+  css: {
+    modules: {
+      globalModulePaths: [/^(?!.*\.module)(.*)\.css/gm],
+    },
+  },
 })
