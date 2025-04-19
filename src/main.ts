758eb30ed5e2f0e7a0554f2e3aa2cd542bacf54f
Benjamin Morrell <BDMorrell@gmail.com>
2025-04-19 13:23:07 -0600

    Remove polyfill

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,6 @@
 /* @refresh reload */
 import makeApplicationRoot from "./app";
 
-import polyfill from "./polyfill";
-// import { attachSubmitEventBlocker } from "./preventFormSubmission";
-
-// Array.from(document.getElementsByTagName("form")).forEach(attachSubmitEventBlocker);
-
-polyfill();
-
 const root = document.getElementById("app");
 
 makeApplicationRoot(root!);
